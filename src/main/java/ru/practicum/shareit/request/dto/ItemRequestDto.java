7cdb998cc01f22e8ba91c3ab6a46712e617ac9b1
Zaven <kalanderyan99@yandex.ru>
2023-07-23 15:46:40 +0300

    fix:ы

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,13 +1,17 @@
 package ru.practicum.shareit.request.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ItemRequestDto {
     @NotNull(message = "Описание вещи не должно быть пустым")
     private String description;
+
 }
